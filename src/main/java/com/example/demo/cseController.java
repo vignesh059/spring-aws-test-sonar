7f5ee9132eecd126ca5f8d5182501704e99b8905
vignesh059 <vijjapuvignesh@gmail.com>
2026-02-28 20:27:49 +0530

    second commit

--- src/main/java/com/example/demo/cseController.java
+++ src/main/java/com/example/demo/cseController.java
@@ -2,7 +2,8 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class cseController {
 	@GetMapping("/cseadd")
 	public int addCse(@RequestParam int a, @RequestParam int b)
--- src/test/java/com/example/demo/cseControllerTest.java
+++ src/test/java/com/example/demo/cseControllerTest.java
@@ -1,16 +1,16 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class cseControllerTest {
 	@Autowired
 	cseController c;
 	@Test
-	void test1() {
+	void test() {
 		int result = c.addCse(2, 3);
 		assertEquals(5, result);
 	}
